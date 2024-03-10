af00d42166a0ba4c9a441f835e80388dd2244a44
sudhakar859 <113663105+sudhakar859@users.noreply.github.com>
2024-03-10 12:23:55 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"  # or "Windows" depending on your requirements
+  reserved            = true
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  site_config {
+    linux_fx_version = "NODE|12-lts"
+  }
+}
+
+output "app_service_url" {
+  value = azurerm_app_service.example.default_site_hostname
+}
